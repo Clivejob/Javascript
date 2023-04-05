de4a985fc43b6011ee24a3acbc78f4b4acfa1229
Lewis Brennan <lewisbrennan444@gmail.com>
2023-04-05 21:54:18 +0100

    new

--- week_06/04_hw_advanced/hw_advanced_start/pangram_finder/pangram_finder.js
+++ week_06/04_hw_advanced/hw_advanced_start/pangram_finder/pangram_finder.js
@@ -1,8 +1,10 @@
 const PangramFinder = function (phrase) {
+  this.phrase = phrase.toLowerCase();
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
 }
 
 PangramFinder.prototype.isPangram = function () {
+  return this.alphabet.every(letter => this.phrase.includes(letter));
 
 }
 
