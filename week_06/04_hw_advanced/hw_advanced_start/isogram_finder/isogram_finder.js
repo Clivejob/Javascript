c0201b4cb6700787a0b0656db013426ae3b6cbdd
Lewis Brennan <lewisbrennan444@gmail.com>
2023-04-06 08:58:22 +0100

    new

--- week_06/04_hw_advanced/hw_advanced_start/isogram_finder/isogram_finder.js
+++ week_06/04_hw_advanced/hw_advanced_start/isogram_finder/isogram_finder.js
@@ -1,5 +1,5 @@
 const IsogramFinder = function (word) {
-
+  this.word = word
 }
 
 IsogramFinder.prototype.isIsogram = function () {
--- week_06/04_hw_advanced/hw_advanced_start/upper_caser/upper_caser.js
+++ week_06/04_hw_advanced/hw_advanced_start/upper_caser/upper_caser.js
@@ -1,10 +1,9 @@
-const PangramFinder = function (phrase) {
-  this.phrase = phrase.toLowerCase();
-  this.alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+const UpperCaser = function (words) {
+  this.words = words
 }
 
-PangramFinder.prototype.isPangram = function () {
-  return this.alphabet.every(letter => this.phrase.includes(letter));
+UpperCaser.prototype.toUpperCase = function () {
+  return this.words.map(word => word.toUpperCase());
 }
 
 module.exports = UpperCaser;
